edb0ee92f5a502378efa710b383fa84f0f635ae6
Harika Thatukuru <harikathatukuru@wirelessprvnat-172-17-224-230.near.illinois.edu>
2016-02-20 00:01:09 -0600

    Initial Commit

---
+++ GestureRecognition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GestureRecognition
+//
+//  Created by Harika Thatukuru on 2/20/16.
+//  Copyright © 2016 Harika Thatukuru. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
